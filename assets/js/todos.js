110f585553df276b3483f74b1463f47a8e51386a
Mahima <mahimasachdeva223@gmail.com>
2018-09-23 16:13:25 +0530

    strike off done items

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,31 @@
 //checking off specific li when clicked
-$("li").click(function(){
-    // $(this).css("color", "gray");
-    // $(this).css("text-decoration", "line-through");
+// $("li").click(function(){
+    // // $(this).css("color", "gray");
+    // // $(this).css("text-decoration", "line-through");
     
-    // above OR :-
-    //make an object with key-val pairs as color and textdecoration
+    // // above OR :-
+    // //make an object with key-val pairs as color and textdecoration
     
-    $(this).css({
-        color: "gray",
-        textDecoration: "line-through"  /* text-Decoration is an error. Hyphens not allowed in property names. So use camelCasing*/
-    });
+    // $(this).css({
+    //     color: "gray",
+    //     textDecoration: "line-through"  /* text-Decoration is an error. Hyphens not allowed in property names. So use camelCasing*/
+    // });
+
+    // if($(this).css("color") === "gray"){    //gray doesn't work. Put rgb here.
+    
+    
+    $("li").click(function(){
+    
+        if($(this).css("color") === "rgb(128, 128, 128)") { 
+            $(this).css({
+                color: "black",
+                textDecoration: "none"
+            });
+        } else {
+            $(this).css({
+                color: "gray",
+                textDecoration: "line-through"
+            });
+        }
+
 });
